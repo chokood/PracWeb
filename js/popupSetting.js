54dac9dcdff4ac7ecf3ba48cbd50c953bcfcbb72
chokood <chokood@gmail.com>
2021-08-18 14:51:56 +0900

    redefined id, class

--- js/popupSetting.js
+++ js/popupSetting.js
@@ -1,11 +1,16 @@
-var modal = document.getElementById("modalTest");
-                        var btn = document.getElementById("btnLogin");
-                        var span = document.getElementsByClassName("close")[0];
-
+var modal = document.getElementById("divpop");
+//var btn = document.getElementById("btnLogin");
+//var span = document.getElementsByClassName("close")[0];
 
 function openPopupSetting()
 {
     console.log('openSetting');
-    var modal = document.getElementById("divpop");
     modal.style.display = "block";
 }
+
+window.onclick = function(event) 
+{
+    if( event.target == modal ) {
+        modal.style.display = "none";
+    }
+};
