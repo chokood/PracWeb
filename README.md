4958a4249201cda260e07bae6eb86938730adb37
chokood <chokood@gmail.com>
2021-08-18 11:24:06 +0900

    dashboard test

--- README.md
+++
@@ -1,2 +0,0 @@
-# PracWeb
-html, css, javascript
---
+++ data/data.js
@@ -0,0 +1,347 @@
+
+gridData = [
+  {
+    cameraName: '전체 카메라 총계',
+    male: 3062,
+    female: 2708,
+    young: 854,
+    youth: 1654,
+    middle: 1400,
+    old: 1400,
+    total: '',
+    mask: '',
+    attention: '',
+    _attributes: {
+      className: {
+        // Add class name on each columns
+        column: {
+          youth: ['blue'],
+          genre: ['blue']
+        }
+      }
+    }
+  },
+  {
+    cameraName: '1층 서관 CT실 주출입구',
+    male: 1080,
+    female: 1000,
+    young: 51,
+    youth: 118,
+    middle: 1080,
+    old: 98,
+    total: 3427,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '2층 서관 CT실 앞 남자화장실편 입구',
+    male: 822,
+    female: '-',
+    young: 34,
+    youth: 365,
+    middle: '-',
+    old: 53,
+    total: 1274,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '3층 서관 CT실 앞 로비',
+    male: 320,
+    female: 202,
+    young: 109,
+    youth: 506,
+    middle: 320,
+    old: 34,
+    total: 1491,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '1층 서관 CT실 주출입구',
+    male: 1080,
+    female: 1000,
+    young: 51,
+    youth: 118,
+    middle: 1080,
+    old: 98,
+    total: 3427,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '2층 서관 CT실 앞 남자화장실편 입구',
+    male: 822,
+    female: '-',
+    young: 34,
+    youth: 365,
+    middle: '-',
+    old: 53,
+    total: 1274,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '3층 서관 CT실 앞 로비',
+    male: 320,
+    female: 202,
+    young: 109,
+    youth: 506,
+    middle: 320,
+    old: 34,
+    total: 1491,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '1층 서관 CT실 주출입구',
+    male: 1080,
+    female: 1000,
+    young: 51,
+    youth: 118,
+    middle: 1080,
+    old: 98,
+    total: 3427,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '2층 서관 CT실 앞 남자화장실편 입구',
+    male: 822,
+    female: '-',
+    young: 34,
+    youth: 365,
+    middle: '-',
+    old: 53,
+    total: 1274,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '3층 서관 CT실 앞 로비',
+    male: 320,
+    female: 202,
+    young: 109,
+    youth: 506,
+    middle: 320,
+    old: 34,
+    total: 1491,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '1층 서관 CT실 주출입구',
+    male: 1080,
+    female: 1000,
+    young: 51,
+    youth: 118,
+    middle: 1080,
+    old: 98,
+    total: 3427,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '2층 서관 CT실 앞 남자화장실편 입구',
+    male: 822,
+    female: '-',
+    young: 34,
+    youth: 365,
+    middle: '-',
+    old: 53,
+    total: 1274,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '3층 서관 CT실 앞 로비',
+    male: 320,
+    female: 202,
+    young: 109,
+    youth: 506,
+    middle: 320,
+    old: 34,
+    total: 1491,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '1층 서관 CT실 주출입구',
+    male: 1080,
+    female: 1000,
+    young: 51,
+    youth: 118,
+    middle: 1080,
+    old: 98,
+    total: 3427,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '2층 서관 CT실 앞 남자화장실편 입구',
+    male: 822,
+    female: '-',
+    young: 34,
+    youth: 365,
+    middle: '-',
+    old: 53,
+    total: 1274,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '3층 서관 CT실 앞 로비',
+    male: 320,
+    female: 202,
+    young: 109,
+    youth: 506,
+    middle: 320,
+    old: 34,
+    total: 1491,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '1층 서관 CT실 주출입구',
+    male: 1080,
+    female: 1000,
+    young: 51,
+    youth: 118,
+    middle: 1080,
+    old: 98,
+    total: 3427,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '2층 서관 CT실 앞 남자화장실편 입구',
+    male: 822,
+    female: '-',
+    young: 34,
+    youth: 365,
+    middle: '-',
+    old: 53,
+    total: 1274,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '3층 서관 CT실 앞 로비',
+    male: 320,
+    female: 202,
+    young: 109,
+    youth: 506,
+    middle: 320,
+    old: 34,
+    total: 1491,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '1층 서관 CT실 주출입구',
+    male: 1080,
+    female: 1000,
+    young: 51,
+    youth: 118,
+    middle: 1080,
+    old: 98,
+    total: 3427,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '2층 서관 CT실 앞 남자화장실편 입구',
+    male: 822,
+    female: '-',
+    young: 34,
+    youth: 365,
+    middle: '-',
+    old: 53,
+    total: 1274,
+    mask: '·',
+    attention: '·',
+  },
+  {
+    cameraName: '3층 서관 CT실 앞 로비',
+    male: 320,
+    female: 202,
+    young: 109,
+    youth: 506,
+    middle: 320,
+    old: 34,
+    total: 1491,
+    mask: '·',
+    attention: '·',
+  }
+
+
+  
+
+];
+
+
+gridData2 = [
+  {
+    eventName: '카운팅', //<string>카운팅, 침입, 배회, 마스크 미착용
+    today: 222, //<number>
+    yesterday: 888, //<number>
+    compYesterday: '-10%', //<string>
+    compWeek: 556, //<number>
+    compLastWeek: '41%', //<string>
+  },
+  {
+    eventName: '침입',
+    today: 68,
+    yesterday: 70,
+    compYesterday: '-3%',
+    compWeek: 556,
+    compLastWeek: '353%',
+  },
+  {
+    eventName: '배회',
+    today: 560,
+    yesterday: 999,
+    compYesterday: '-44%',
+    compWeek: 556,
+    compLastWeek: '-7%',
+  },
+  {
+    eventName: '마스크 미착용',
+    today: 23,
+    yesterday: 22,
+    compYesterday: '5%',
+    compWeek: 556,
+    compLastWeek: '-59%',
+  },
+
+];
+
+
+
+// //카메라 별 이벤트 테이블
+// gridData = [
+//   {
+//     cameraName: '1층 서관 CT실 주출입구', //<string> 카메라 이름
+//     male: 1080,                          //<number> 각 이벤트 검출 수 
+//     female: 1000,                        //<number>
+//     young: 51,                           //<number>
+//     youth: 118,                          //<number>
+//     middle: 1080,                        //<number>
+//     old: 98,                             //<number>
+//     total: 3427,                         //<number>
+//     mask: 0,                             //<number>
+//     attention: 0,                        //<number>
+//   }
+// ];
+
+// //이벤트 별 검출 수 등등
+// gridData2 = [
+//   {
+//     eventName: '카운팅',                 //<string>카운팅, 침입, 배회, 마스크 미착용
+//     today: 222,                          //<number>
+//     yesterday: 888,                      //<number>
+//     compYesterday: '-10%',               //<string>
+//     compWeek: 556,                       //<number>
+//     compLastWeek: '41%',                 //<string>
+//   }
+// ];
