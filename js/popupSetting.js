bf3e56015a4105e37bdba4cf57f74efe73ca19b7
chokood <chokood@gmail.com>
2021-08-18 14:42:17 +0900

    popup

--- js/popupSetting.js
+++ js/popupSetting.js
@@ -1,6 +1,11 @@
+var modal = document.getElementById("modalTest");
+                        var btn = document.getElementById("btnLogin");
+                        var span = document.getElementsByClassName("close")[0];
+
+
 function openPopupSetting()
 {
     console.log('openSetting');
-    var modal = document.getElementById("indexWrap");
+    var modal = document.getElementById("divpop");
     modal.style.display = "block";
 }
