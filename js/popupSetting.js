ae5816518a22b1defec2128ec1dc81c38db0507e
chokood <chokood@gmail.com>
2021-08-18 16:06:58 +0900

    das

--- js/popupSetting.js
+++ js/popupSetting.js
@@ -1,13 +1,13 @@
-var modal = document.getElementById("modal");
-
 function openPopupSetting()
 {
     console.log('openSetting');
+    var modal = document.getElementById("modal");
     modal.style.display = "block";
 }
 
 window.onclick = function(event) 
 {
+    var modal = document.getElementById("modal");
     if( event.target == modal ) {
         modal.style.display = "none";
     }
